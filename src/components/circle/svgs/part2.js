166a0a7ae98d97746d9d728e2100119bb40c6801
Hillel-Nagid <hillel123123@gmail.com>
2020-08-02 22:39:09 +0300

    circle and job is done

---
+++ src/components/circle/svgs/part2.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './part2.css';
+
+const part2 = () => {
+	return (
+		<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 476 476'>
+			<g id='Layer_2' data-name='Layer 2'>
+				<g id='circle'>
+					<circle class='cls-1' cx='238' cy='238' r='231.5' />
+				</g>
+			</g>
+		</svg>
+	);
+};
+
+export default part2;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,22 @@
-import React from "react"
+import React from 'react';
+import Circle from '../components/circle/Circle.js';
+import '../styles/index.css';
+import write from '../helpers';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Main() {
+	const job = 'FRONTEND DEVELOPER';
+	const jobName = useState('');
+	useEffect(() => {
+		const timer = setTimeout(() => write(jobName, 0, job, 100), 7000);
+		return () => clearTimeout(timer);
+		// eslint-disable-next-line
+	}, []);
+	return (
+		<div className='main'>
+			<Circle></Circle>
+			<h1 className='job'>{jobName[0]}</h1>
+		</div>
+	);
 }
